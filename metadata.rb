f703cb402ee982a04bbdb121db7c7756ef57b2a6
Eido NABESHIMA <closer009@gmail.com>
2013-08-06 22:53:06 +0900

    initialized

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'cookbook-eccube'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures cookbook-eccube'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
