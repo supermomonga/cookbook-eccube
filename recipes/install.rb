f7f22c0ad07cc2fd749ca49e6ef8fd7835f7d92c
Eido NABESHIMA <closer009@gmail.com>
2013-08-15 19:26:23 +0900

    reload対応

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,21 +1,23 @@
-%w(welcome step0 step0_1 step1 step2 step3 step4 complete).each do |mode|
-  execute "curl_install_#{mode}" do
-    command "curl http://localhost/install/ -X POST -d @- < /tmp/install_params_#{mode} "
-    not_if { ::File.exists? "#{node['eccube']['dir']}/data/config/config.php" }
-    action :nothing
+execute "curl_install" do
+  curl_commands = %w(welcome step0 step0_1 step1 step2 step3 step4 complete).map do |mode|
+    "(echo \"mode=#{mode}&`cat /tmp/install_params`\") | curl http://localhost/install/ -X POST -d @-; "
   end
+  command curl_commands.join
+  not_if { ::File.exists? "#{node['eccube']['dir']}/data/config/config.php" }
+  action :nothing
+end
 
-  template "/tmp/install_params_#{mode}" do
-    source "install_params.erb"
-    variables(:mode => mode)
-    not_if { ::File.exists? "#{node['eccube']['dir']}/data/config/config.php" }
-    notifies :run, "execute[curl_install_#{mode}]", :immediately
-  end
+template "/tmp/install_params" do
+  source "install_params.erb"
+  not_if { ::File.exists? "#{node['eccube']['dir']}/data/config/config.php" }
+  notifies :run, "execute[curl_install]", :immediately
 end
 
 execute "remove_install_dir" do
   command "rm #{node['eccube']['dir']}/html/install/index.php"
-  only_if { ::File.exists? "#{node['eccube']['dir']}/html/install/index.php" }
+  only_if do
+    ::File.exists?("#{node['eccube']['dir']}/data/config/config.php") && ::File.exists?("#{node['eccube']['dir']}/html/install/index.php")
+  end
   action :run
 end
 
